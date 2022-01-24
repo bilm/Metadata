86328fe199d185d0edc8d9e86752f716e68dae66
Bil Moorhead <bil@alitheon.com>
2022-01-24 13:22:11 -0800

    updated the minimum maxOS version to 11

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Metadata",
-	platforms: [ .macOS(.v10_15), .iOS(.v14) ],
+	platforms: [ .macOS(.v11), .iOS(.v14) ],
     products: [
         .library(name: "Metadata", targets: ["Metadata"]),
     ],
